375ddb33cd511289221ac3a27a50f357baa19564
Jesse Feng <fxm0016@126.com>
2021-09-20 23:14:52 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,21 @@
 const { getESLintConfig } = require('@iceworks/spec');
 
-// https://www.npmjs.com/package/@iceworks/spec
-module.exports = getESLintConfig('rax');
+const eslint = getESLintConfig('rax');
+let {rules, globals} = eslint;
+if(!rules) {
+  rules = {};
+  eslint.rules = rules;
+}
+if(!globals) {
+  globals = {};
+  eslint.globals = globals;
+}
+rules['no-sequences'] = 'off';
+rules['no-param-reassign'] = 'off';
+rules['no-cond-assign'] = 'off';
+rules['@iceworks/best-practices/no-http-url'] = 'off';
+rules['default-case'] = 'off';
+
+globals.dd = true;
+
+module.exports = eslint;
