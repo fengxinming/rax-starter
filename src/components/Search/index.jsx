a57bacc43bede135d933e06be212db1fc4723dfe
Jesse Feng <fxm0016@126.com>
2021-09-25 22:45:29 +0800

    Create index.jsx

---
+++ src/components/Search/index.jsx
@@ -0,0 +1,11 @@
+import { Search } from '@alifd/meet';
+import { createICComponent } from '@/common/component-util';
+
+import './index.scss';
+
+const CSearch = createCComponent(
+  Search,
+  'c-search-panel',
+);
+
+export default CSearch;
